ec706116672914fdee1d259d9379672c3268177c
Your Name <bvbrunik@gmail.com>
2023-02-02 16:11:08 +0100

    Added error handling.

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import express, { json } from "express"
 import cors from 'cors';
 import 'express-async-errors'
+import {handleError, ValidationError} from "./utils/errors";
 
 const app =express();
 
@@ -10,6 +11,14 @@ app.use(cors({
 }));
 
 app.use(json());
+
+//Routes...
+
+app.get('/',async (req,res)=> {
+    throw new ValidationError('Damn')
+})
+
+app.use(handleError);
 
 app.listen(3001,'0.0.0.0',()=> {
     console.log('Listening on port http://localhost:3001')
