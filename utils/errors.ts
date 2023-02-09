8e7499acff83ddd666439f97a5e32396f056601c
Your Name <bvbrunik@gmail.com>
2023-02-09 22:40:23 +0100

    Added another tests anc checked

--- utils/errors.ts
+++ utils/errors.ts
@@ -1,14 +1,11 @@
 import {NextFunction, Request, Response} from "express";
-
-export class ValidationError extends Error {}
-
-export const handleError = (err:Error,req:Request,res: Response,next:NextFunction)=> {
-    console.log(err);
-
-    res.
-        status(err instanceof ValidationError ? 400 : 500)
+export class ValidationError extends Error {
+}
+export const handleError = (err: Error, req: Request, res: Response, next: NextFunction) => {
+    console.error(err);
+    res
+        .status(err instanceof ValidationError ? 400 : 500)
         .json({
-            message: err instanceof ValidationError ? err.message : 'Sorry,please try again later',
+            message: err instanceof ValidationError ? err.message : 'Sorry, please try again later.',
         });
 }
-
